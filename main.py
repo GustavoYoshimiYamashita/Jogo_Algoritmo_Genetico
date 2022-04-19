a80921b1568d4597364679257eda6d2197fa2ef1
GustavoYoshimiYamashita <gustavo.yamashita@outlook.com>
2022-04-19 10:20:49 -0300

    Apaguei um espaço para teste de commit.

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ LinkedIn: https://www.linkedin.com/in/gustavo-yamashita/
 
 '''
 
-
 import pygame
 from pygame.locals import *
 from pygame.transform import *
@@ -26,26 +25,42 @@ white = (255, 255, 255)
 vertical = 600
 horizontal = 600
 
+
 angle = 30
 
+# Inicializando a tela do Pygame
 surface = pygame.display.set_mode((horizontal, vertical))
+
+# carregando a imagem para imprimir na tela
 image = pygame.image.load(r'C:\Users\gusta\Desktop\VideoAula1\imagens\robo-removebg-preview.png')
-IMAGE_SMALL = image #pygame.transform.scale(image, (133.5, 116.75))
+
+# Modificando o tamanho da imagem
+IMAGE_SMALL = pygame.transform.scale(image, (133.5, 116.75))
+
 
 while jogo:
+
+    # Preenchendo a tela inicialmente como preto
     surface.fill(black)
+
+    # Para cada evento dentro do Pygame
     for event in pygame.event.get():
+        # Caso detectar o clique no "X" do display, então encerrar o processo
         if event.type == QUIT:
             pygame.quit()
             exit()
+        # Caso detectar o clique na tecla de seta para direita e para esquerda, aumentar ou diminuir o ângulo
         if event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT:
                 angle+=1
             if event.key == pygame.K_LEFT:
                 angle-=1
 
+    # Rotacionando a imagem
     rotated = pygame.transform.rotate(IMAGE_SMALL, angle)
     surface.blit(rotated, (0, 0))
+
+    # Atualizando a imagem
     pygame.display.update()
     pygame.display.flip()
 
